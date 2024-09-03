11b704ad37aa6d4a4adf06df9a944c7c722aefad
AliAlTaweel <engalihaitham@gmail.com>
2024-09-03 21:02:00 +0300

    v2

--- 04-/src/App.jsx
+++ 04-/src/App.jsx
@@ -1,20 +1,35 @@
 import Display from "./Display";
 import Button from "./Button";
+import History from "./History";
 import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(90);
-  const plusOne = () => setCounter(counter + 1);
-  const setTozero = () => setCounter(0);
-  const minusOne = () => setCounter(counter - 1);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+  const handleLeftClick = () => {
+    const nLeft = left + 1;
+    setLeft(nLeft);
+    setAll(allClicks.concat("L"));
+    setTotal(nLeft + right);
+  };
+  const handleRightClick = () => {
+    const nRight = right + 1;
+    setRight(nRight);
+    setAll(allClicks.concat("R"));
+    setTotal(left + nRight);
+  };
 
   return (
-    <>
-      <Display counter={counter} />
-      <Button onClick={plusOne} text="Plus" />
-      <Button onClick={setTozero} text="Reset" />
-      <Button onClick={minusOne} text="Minus" />
-    </>
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      
+      <History total={allClicks}/>
+    </div>
   );
 }
 
---
+++ 04-/src/History.jsx
@@ -0,0 +1,16 @@
+
+const History = (props) =>{
+    if(props.total === 0){
+    return (
+        <div>
+            <p> the app is used by pressing the buttons</p>
+        </div>
+    )}
+    return (
+        <div>
+            button press history: {props.total.join(" ,")}
+        </div>
+    )
+
+}
+export default History;
